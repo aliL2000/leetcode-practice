7ac4991cb9b46df62fadb6c382273f1d59c73154
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-13 21:26:54 -0700

    Adding ugly_prime solution in java

---
+++ questions/easy/ugly_prime.java
@@ -0,0 +1,16 @@
+package questions.easy;
+
+class Solution {
+    public boolean isUgly(int n) {
+        if (n==0) return false;
+        int[] primes = {2,3,5};
+        for(int i=0;i<primes.length;i++){
+            while (n%primes[i] == 0){
+                n=n/primes[i];
+            }
+            if (n == 1)
+                return true;
+        }
+        return false;
+    } 
+}
