15f4d49adab1395e0fb0da243e7b52723a5c5ab4
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-14 20:19:54 -0700

    Upload group_anagrams

---
+++ questions/medium/group_anagrams.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res=defaultdict(list)
+        for str in strs:
+            count = [0] * 26;
+            for character in str:
+                count[ord(character)-ord("a")] += 1
+            res[tuple(count)].append(str)
+        return res.values()
