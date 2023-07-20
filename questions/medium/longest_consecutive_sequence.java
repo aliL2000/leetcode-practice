6f9c16895165398747d9e037852df9847a2176ad
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-20 13:58:02 -0700

    Adding new question

---
+++ questions/medium/longest_consecutive_sequence.java
@@ -0,0 +1,27 @@
+//Question can be found here:
+//https://leetcode.com/problems/top-k-frequent-elements/
+
+package questions.medium;
+import java.util.Arrays;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        Arrays.sort(nums);
+        int overall_max = 0;
+        int current_max = 0;
+        for (int i = 0; i< nums.length-1;i++){
+            if (nums[i] + 1 == nums[i+1] || nums[i] == nums[i+1]){
+                if (nums[i] != nums[i+1])
+                    current_max=current_max+1;
+                if (current_max>overall_max)
+                    overall_max=current_max;
+            }
+            else{
+                current_max=0;
+            }
+        }
+        return overall_max+1;
+    }
+}
