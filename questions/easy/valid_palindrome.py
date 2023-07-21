ef8ba51a4a41fd7ff6ab5408652e21027e9fcb5b
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-21 14:50:04 -0700

    Updating solution

--- questions/easy/valid_palindrome.py
+++ questions/easy/valid_palindrome.py
@@ -4,3 +4,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         filtered_string = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
         return filtered_string == filtered_string[::-1]
+        #alternative solution using two pointers on the string
+        # string_length = len(filtered_string)
+        # for i in range(int(string_length/2)):
+        #     if filtered_string[i]!=filtered_string[string_length-i-1]:
+        #         return False
+        # return True
