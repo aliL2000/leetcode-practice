759dfc47e0bc7c8b52907bc0f62039f3858b0124
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-21 12:11:38 -0700

    Uploading product of array except self

---
+++ questions/medium/product_array_except_self.java
@@ -0,0 +1,23 @@
+//Question can be found here:
+//https://leetcode.com/problems/product-of-array-except-self/
+
+package questions.medium;
+import java.util.Arrays;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        Arrays.fill(res, 1);
+        int prefix = 1;
+        for(int i = 0; i < nums.length; i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for(int i = nums.length -1; i>-1;i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}
