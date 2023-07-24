ffd315ef206082264ccab6b2af313757fcc27cae
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-24 14:43:46 -0700

    adding new problem

---
+++ questions/medium/two_sum_2.java
@@ -0,0 +1,24 @@
+//Question can be found here:
+//https://leetcode.com/problems/two-sum/
+package questions.medium;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Integer[] array = new Integer[nums.length];
+        int[] idx = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int needed = target - nums[i];
+            if(Arrays.asList(array).contains(needed))
+                {
+                    int id = Arrays.asList(array).indexOf(needed);
+                    idx[0] = id+1;
+                    idx[1] = i+1;
+                    return idx;
+                }
+            array[i]=nums[i];
+        }
+        return idx;
+    }
+}
