d7c4b13af881c16058b543504b0d4aae43302abe
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-08-07 16:04:31 -0700

    Adding efficient solution

--- questions/medium/valid_sudoku.py
+++ questions/medium/valid_sudoku.py
@@ -1,5 +1,27 @@
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        #This solution uses sets to create a unique list of numbers, and if it finds a match, returns false
+        # rows = defaultdict(set)
+        # cols = defaultdict(set)
+        # squares = defaultdict(set)
+        # for i in range(9):
+        #     for j in range(9):
+        #         if (board[i][j] == "."):
+        #             continue
+        #         elif (board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[(i // 3, j //3)]):
+        #             return False
+        #         else:
+        #             rows[i].add(board[i][j])
+        #             cols[j].add(board[i][j])
+        #             squares[(i // 3, j // 3)].add(board[i][j])
+        # return True
+
+
+        #This is my solution, it doesn't have good runtime or memory usage, but made the most sense to me logically
+        #MY SOLUTION
         for i in range(9):
             for j in range(9):
                 if board[i][j].isnumeric() and (not isValidColumn(board,i,j) or not isValidRow(board,i,j) or not isValidBox(board,i,j)):
