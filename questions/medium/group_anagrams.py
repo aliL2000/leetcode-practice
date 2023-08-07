68d11267a17f48e6405ef39f29ea9811e8567894
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-08-07 14:55:15 -0700

    Adding revisions

--- questions/medium/group_anagrams.py
+++ questions/medium/group_anagrams.py
@@ -11,3 +11,13 @@ class Solution:
                 count[ord(character)-ord("a")] += 1
             res[tuple(count)].append(str)
         return res.values()
+    
+        #This solution is a lot faster, we add a dictionary with key value pairs, with each string appended to it's respective key, then we return the values
+        # anagrams = {}
+        # for word in strs:
+        #     temp = ''.join(sorted(word))
+        #     if temp in anagrams:
+        #         anagrams[temp].append(word)
+        #     else:
+        #         anagrams[temp] = [word]
+        # return anagrams.values()
