4c1381fb4b1f9b43395f492e6580ef1c6e73914e
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-05 10:34:44 -0400

    Add Binay searh

---
+++ questions/easy/binary_search.java
@@ -0,0 +1,19 @@
+package questions.easy;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int index = (left + right) / 2;
+            if (nums[index] < target) {
+                left = index + 1;
+            } else if (nums[index] > target) {
+                right = index - 1;
+            } else {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
