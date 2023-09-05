a1b2294db50c6312aa90ae744f9bbc3629446aa7
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-05 11:31:16 -0400

    Adding link

--- questions/medium/search_2d_matrix.py
+++ questions/medium/search_2d_matrix.py
@@ -1,3 +1,6 @@
+#Question can be found here:
+#https://leetcode.com/problems/search-a-2d-matrix/
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows = len(matrix)
