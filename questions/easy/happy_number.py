fd97d75064c4e78293adea37fe68a2e53206f9c0
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-09 13:32:53 -0400

    Optimized solution

--- questions/easy/happy_number.py
+++ questions/easy/happy_number.py
@@ -3,18 +3,33 @@
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        iterations = []
-        iterations.append(n)
-        while(1 not in iterations):
-            temp_sum = 0
-            string_number=list(str(n))
-            for number in string_number:
-                temp_sum+=int(number)*int(number)
-            if 1 in iterations:
-                return True
-            elif temp_sum in iterations:
-                return False
-            else:
-                iterations.append(temp_sum)
-                n=temp_sum
+
+        #MY SOLUTION
+        #Really not time efficient whatsoever, if I ever run into a loop, I check to see if it's already there. Should use a set tbh
+        # iterations = []
+        # iterations.append(n)
+        # while(1 not in iterations):
+        #     temp_sum = 0
+        #     string_number=list(str(n))
+        #     for number in string_number:
+        #         temp_sum+=int(number)*int(number)
+        #     if 1 in iterations:
+        #         return True
+        #     elif temp_sum in iterations:
+        #         return False
+        #     else:
+        #         iterations.append(temp_sum)
+        #         n=temp_sum
+        # return True
+
+        #This code below does exactly what I did, but it's far simpler and 
+        set_of_no=set()
+        while n!=1: 
+            #Convert the input number to a string, and using list comprehesion, sum the values of each digit
+            n=sum([int(i)**2 for i in str(n)])
+            #Check to see if it's in the set, and if it is, return False, else return True after reaching 1
+            if n in set_of_no:
+                return False 
+            set_of_no.add(n) 
         return True
+    
