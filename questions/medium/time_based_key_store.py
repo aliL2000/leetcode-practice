7cb83f86c00a903737527e4147391d1545ed90e6
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-11 21:41:59 -0400

    Adding non-original solution

---
+++ questions/medium/time_based_key_store.py
@@ -0,0 +1,21 @@
+class TimeMap:
+
+    def __init__(self):
+        self.timemap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.timemap:
+            self.timemap[key] = []
+        self.timemap[key].append([value,timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.timemap.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
