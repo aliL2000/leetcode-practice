a9e7a9eebce26b98e28f89deaf91c1bf396e400f
thenewguy321 <aliladha2000@gmail.com>
2023-09-13 17:15:19 -0700

    Adding link

--- questions/medium/longest_substring_without_repeating_chars.py
+++ questions/medium/longest_substring_without_repeating_chars.py
@@ -1,3 +1,6 @@
+#Question can be found here:
+#https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s)==0:
