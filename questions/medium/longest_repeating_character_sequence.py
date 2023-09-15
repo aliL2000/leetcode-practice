1251d0165f8726dbf95e171be4c9a0f5281dc906
thenewguy321 <aliladha2000@gmail.com>
2023-09-14 22:28:21 -0400

    Adding quesiton

---
+++ questions/medium/longest_repeating_character_sequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counts = {}
+        maxf = 0
+        l = 0
+        for r, ch in enumerate(s):
+            counts[ch] = 1 + counts.get(ch, 0)
+            maxf = max(maxf, counts[ch])
+            if maxf + k < r - l + 1:
+                counts[s[l]] -= 1
+                l += 1
+        return len(s) - l
