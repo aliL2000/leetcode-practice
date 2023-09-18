cc62ab34e88241c652608ef1fad69a053828cc34
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-18 15:10:11 -0400

    revise

--- questions/medium/permutation_string.py
+++ questions/medium/permutation_string.py
@@ -17,28 +17,29 @@ class Solution:
         # Use a hashmap to store a key-count pair of the number of characters, then as we go through s2, create a sliding window of current chars
         if len(s1)>len(s2):
             return False
-        s1count,s2count = [0]*26,[0]*26
+        s1Count,s2Count = [0]*26,[0]*26
         for i in range(len(s1)):
-            s1count[ord(s1[i])- ord('a')] += 1
-            s2count[ord(s2[i])- ord('a')] += 1
-        matches = 0
-        for i in range(26):
-            matches += (1 if s1count[i] == s2count[i] else 0)
+            s1Count[ord(s1[i])- ord('a')] += 1
+            s2Count[ord(s2[i])- ord('a')] += 1
+        
+        matches = sum(map(lambda i: s1Count[i] == s2Count[i], range(26)))
 
-        l = 0
-        for r in range(len(s1),len(s2)):
+        for l in range(len(s2) - len(s1)):
             if matches == 26:
                 return True
-            index = ord(s2[r]) - ord('a')
-            s2count[index] +=1
-            if s1count[index] == s2count[index]:
-                matches+=1
-            elif s1count[index] + 1 == s2count[index]:
-                matches-=1
-        
-            index = ord(s2[l]) - ord('a')
-            s2count[index] +=1
-            if s1count[index] == s2count[index]:
-                matches+=1
-            elif s1count[index] + 1 == s2count[index]:
-                matches-=1
+
+            i = ord(s2[l]) - ord("a")
+            s2Count[i] -= 1
+            if s2Count[i] == s1Count[i]:
+                matches += 1
+            elif s2Count[i] == s1Count[i] - 1:
+                matches -= 1
+
+            i = ord(s2[l + len(s1)]) - ord("a")
+            s2Count[i] += 1
+            if s2Count[i] == s1Count[i]:
+                matches += 1
+            elif s2Count[i] == s1Count[i] + 1:
+                matches -= 1
+
+        return matches == 26
