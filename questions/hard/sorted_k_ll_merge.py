6240004da5e0788943ee2a27a0f849c42c4dbcd7
aliL2000 <aliladha2000@gmail.com>
2023-09-29 08:43:04 -0400

    Adding hard question

---
+++ questions/hard/sorted_k_ll_merge.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists)>1:
+            mergedList = []
+            for i in range(0, len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                mergedList.append(self.mergeList(l1,l2))
+            lists = mergedList
+        return lists[0]
+
+    def mergeList(self,list1,list2):
+        if not list1 or not list2:
+            return list1 if list1 else list2
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+        list1.next = self.mergeList(list1.next,list2)
+        return list1
